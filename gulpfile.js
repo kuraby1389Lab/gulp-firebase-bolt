bc455c8813ebe8e42fa253638e265a5f5fe4f0d6
Khoa Tran <48739283+khoa-m3dicine@users.noreply.github.com>
2024-08-20 10:30:15 +1000

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var firebase = require('./index');
 
 gulp.task('default', function(){
-  gulp.src('./tests/test.bolt')
+  return gulp.src('./tests/test.bolt')
   .pipe(firebase())
   .pipe(gulp.dest('./output/'));
 });
